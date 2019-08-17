f3af072b498e7d0b37efe8a709bff599b6553665
johan villamil <johinsdev@gmail.com>
2019-08-17 14:38:41 -0500

    qadd

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+
+  plugins: ["@typescript-eslint"],
+
+  extends: [
+    "react-app",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+
+  env: {
+    es6: true,
+    node: true,
+  },
+
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2018,
+  },
+
+  rules: {
+    "jsx-a11y/anchor-is-valid": "off",
+    "@typescript-eslint/no-angle-bracket-type-assertion": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+  },
+
+  globals: {},
+};

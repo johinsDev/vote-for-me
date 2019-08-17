d2ee038e6e98186d24fdaf29aa79f95fddd0f93d
johan villamil <johinsdev@gmail.com>
2019-08-17 14:39:59 -0500

    add eslint for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ const App: React.FC = (): React.ReactElement => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
+      <p>Probando</p>
       <a
         className="App-link"
         href="https://reactjs.org"
